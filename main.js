569d301ba1b5df4845f21117645310e8233e102e
ahakanergun <ahakanergun@gmail.com>
2016-12-23 16:06:43 +0300

    Arayüz güncellendi

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var menubar = require('menubar')
 
-var mb = menubar()
+var mb = menubar({
+  width: 575,
+  height: 430,
+  resizable: false
+})
 
 mb.on('ready', function ready () {
   console.log('app is ready')

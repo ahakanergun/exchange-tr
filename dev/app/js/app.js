ca8458354353649b34d70452713cb4b7d7f8c781
ahakanergun <ahakanergun@gmail.com>
2016-12-26 15:21:44 +0300

    görsel düzenlemeler

--- dev/app/js/app.js
+++ dev/app/js/app.js
@@ -1,25 +1,71 @@
 $(document).ready(function() {
 	moment.locale('tr');
+	getCurrencyData();
+
 	$('#today').text();
 	$('#today').text(moment().format('LL') + ', ' + moment().format('dddd'));
+	$('#today').append('<span class="clock"> ' + moment().format('H:mm:ss') + '</span>');
 
-	axios.get('http://api.fixer.io/latest?base=USD')
-		.then(function(response) {
-			$('#ustl').text();
-			$('#ustl').text((response.data.rates.TRY).toFixed(3));
-		})
-		.catch(function(error) {
-			console.log(error);
-		});
+	function update() {
+		$('.clock').remove();
+		$('#today').append('<span class="clock"> ' + moment().format('H:mm:ss') + '</span>');
+	}
 
-	axios.get('http://api.fixer.io/latest?base=EUR')
-		.then(function(response) {
-			$('#eutl').text();
-			$('#eutl').text((response.data.rates.TRY).toFixed(3));
-		})
-		.catch(function(error) {
-			console.log(error);
-		});
+	setInterval(update, 1000);
+
+	function getCurrencyData() {
+		axios.get('http://www.doviz.com/api/v1/currencies/USD/latest')
+			.then(function(response) {
+				$('#ustl').text();
+				$('#ustl').text((response.data.selling).toFixed(4));
+				$('#usSellBuy').text();
+				$('#usSellBuy').text((response.data.buying).toFixed(4) + ' / ' + (response.data.selling).toFixed(4));
+				$('#dollarRateBox').removeClass();
+				if (response.data.change_rate < 0) {
+					$('#dollarRateBox').addClass("card card-border-danger text-xs-center");
+				} else {
+					$('#dollarRateBox').addClass("card card-border-success text-xs-center");
+				}
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+
+		axios.get('http://www.doviz.com/api/v1/currencies/EUR/latest')
+			.then(function(response) {
+				$('#eutl').text();
+				$('#eutl').text((response.data.selling).toFixed(4));
+				$('#euSellBuy').text();
+				$('#euSellBuy').text((response.data.buying).toFixed(4) + ' / ' + (response.data.selling).toFixed(4));
+				$('#euroRateBox').removeClass();
+				if (response.data.change_rate < 0) {
+					$('#euroRateBox').addClass("card card-border-danger text-xs-center");
+				} else {
+					$('#euroRateBox').addClass("card card-border-success text-xs-center");
+				}
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+
+		axios.get('http://www.doviz.com/api/v1/golds/gram-altin/latest')
+			.then(function(response) {
+				$('#altl').text();
+				$('#altl').text((response.data.selling).toFixed(2));
+				$('#alSellBuy').text();
+				$('#alSellBuy').text((response.data.buying).toFixed(2) + ' / ' + (response.data.selling).toFixed(2));
+				$('#goldRateBox').removeClass();
+				if (response.data.change_rate < 0) {
+					$('#goldRateBox').addClass("card card-border-danger text-xs-center");
+				} else {
+					$('#goldRateBox').addClass("card card-border-success text-xs-center");
+				}
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+	}
+	setInterval(getCurrencyData, 60000);
 });
 
 $("#toDollar").keyup(function(event) {
@@ -34,7 +80,6 @@ $("#toEuro").keyup(function(event) {
 	$('#euroResult span').text((turkLira / euroRate).toFixed(2));
 });
 
-
 $("#dollarToLira").keyup(function(event) {
 	var dollarRate = parseFloat($('#ustl').text()),
 		dollar = $("#dollarToLira").val();
@@ -45,6 +90,38 @@ $("#euroToLira").keyup(function(event) {
 	var euroRate = parseFloat($('#eutl').text()),
 		euro = $("#euroToLira").val();
 	$('#euroToLiraResult span').text((euro * euroRate).toFixed(2));
+});
+
+$("#dollarRateBox").click(function() {
+	if ($('.usSellBuyBlock').css('display') == 'none') {
+		$(".usSellBuyBlock").show();
+		$(".dollarConverter").show();
+		$(".usDefaultBlock").hide();
+	} else {
+		$(".usSellBuyBlock").hide();
+		$(".dollarConverter").hide();
+		$(".usDefaultBlock").show();
+	}
+});
+
+$("#euroRateBox").click(function() {
+	if ($('.euSellBuyBlock').css('display') == 'none') {
+		$(".euSellBuyBlock").show();
+		$(".euDefaultBlock").hide();
+	} else {
+		$(".euSellBuyBlock").hide();
+		$(".euDefaultBlock").show();
+	}
+});
+
+$("#goldRateBox").click(function() {
+	if ($('.alSellBuyBlock').css('display') == 'none') {
+		$(".alSellBuyBlock").show();
+		$(".alDefaultBlock").hide();
+	} else {
+		$(".alSellBuyBlock").hide();
+		$(".alDefaultBlock").show();
+	}
 });
 
 $('button.close').click(function() {
